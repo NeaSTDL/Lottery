d9a719f0b58d615893c3edbbd1bb71038be70a77
Erick Agrazal <erick@nextcenturypublishing.com>
2016-06-15 09:02:19 -0500

    asd!

--- imports/api/profile.js
+++ imports/api/profile.js
@@ -5,9 +5,10 @@ export const Profile = new Mongo.Collection('profile');
 
 if(Meteor.isServer){
 	Meteor.publish("userProfile", function(user_id){
-                       console.log('u', user_id);
-                       //console.log('2', this.userId);
-                       console.log('t', Profile.findOne({owner: user_id}));
-		return Profile.find({owner: user_id});
+                return Profile.find({owner: user_id});
 	});
+            Meteor.publish("allUserProfiles", function(){
+                console.log(Profile.find());
+                return Profile.find();
+            });
 }
